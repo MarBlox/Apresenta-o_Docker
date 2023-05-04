95bd2a10705d72e7b2133f4caf7e208c140075e4
Marco Melo <marcomiguelmedeiros2006@gmail.com>
2023-05-04 20:16:26 +0000

    teste

--- docker/volumes/python/app.py
+++ docker/volumes/python/app.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 r = redis.Redis(host='redis', port=6379)
 
 @app.route('/')
-def hello_world():
+def Ta_Feito():
     r.incr('hits')
     count = r.get('hits').decode('utf-8')
-    return f'Hello World! This page has been viewed {count} times.'
+    return f'Ta funcionando prof ja atualizou {count} vezes.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
