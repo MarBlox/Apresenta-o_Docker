855d1303f5faa68ffc99d76cd35b8b896febee3f
MarBlox <marcomiguelmedeiros2006@gmail.com>
2023-05-09 11:18:56 +0000

    .

--- docker/volumes/python/app.py
+++ docker/volumes/python/app.py
@@ -4,11 +4,7 @@ import redis
 app = Flask(__name__)
 r = redis.Redis(host='redis', port=6379)
 
-@app.route('/')
-def Ta_Feito():
-    r.incr('hits')
-    count = r.get('hits').decode('utf-8')
-    return f'Ta funcionando prof ja atualizou {count} vezes.'
+print("Ta Feito")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
